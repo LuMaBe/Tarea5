7e21ea6fd33d656a3b29bbe60c40398d405941e2
santiago <santicua2000@gmail.com>
2019-05-31 14:15:42 -0300

    Update avl.cpp

--- avl.cpp
+++ avl.cpp
@@ -68,13 +68,19 @@ static avl_ultimo avl_min_rec(nat h, nat primero) {
     res.avl = NULL;
     res.ultimo = primero - 1;
   } else if (h == 1) {
-    info_t info = crear_info(1, NULL);
+    char *frase = new char[1];
+    frase[0] = \0;
+    info_t info = crear_info(1, frase);
     insertar_en_avl(info, res.avl);
     res.ultimo = primero;
   } else {
 
     res.avl->izq = avl_min_rec(h-1, primero);
     res.avl->der = avl_min_rec(h-2, primero);
+    res.avl->izq = (avl_min_rec(h-1, primero))->avl;
+    res.avl->der = (avl_min_rec(h-2, primero))->avl;
+    
+    
   }
   return res;
 };
